edcf963d74cfbd0b8170dc1a4100da02c814d35f
Gabriel Luciano <Gluciano@Gabriels-MacBook-Pro.local>
2018-03-13 02:58:13 -0400

    first commit

---
+++ js/config/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD_3_GCv86nZObylHJJ4dGAk-cRIle927Q",
+  authDomain: "fir-chat-1e9b2.firebaseapp.com",
+  databaseURL: "https://fir-chat-1e9b2.firebaseio.com",
+  projectId: "fir-chat-1e9b2",
+  storageBucket: "fir-chat-1e9b2.appspot.com",
+  messagingSenderId: "670908790433"
+};
+firebase.initializeApp(config);
